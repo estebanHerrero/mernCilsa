7d92cc59fccb8e506193ee6b5ba598f3ebda4eeb
stevendev43 <herstevendev@gmail.com>
2024-11-20 17:31:29 -0300

    listado de tareas en el front

--- back/server.js
+++ back/server.js
@@ -25,5 +25,5 @@ app.get("/", (req, res) => {
 }) 
 
 app.listen(3000, () => {
-    console.log(" ..(oO Escuchando el puerto 3000 Oo)..");
+    console.log(" ..oO) Escuchando el puerto 3000 (Oo..");
 })
--- front/src/Tareas.jsx
+++ front/src/Tareas.jsx
@@ -1,13 +1,16 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from 'axios';
 
 
 function Tareas() {
 
-
+    const [tareas, setTareas] = useState([])
     useEffect(() => {
         axios.get('http://localhost:3000/')
-        .then(res => console.log(res))
+        .then(res => {
+            setTareas(res.data);
+            console.log(res.data);
+        })
         .catch(err => console.log(err));
     }, [])
 
@@ -16,19 +19,31 @@ function Tareas() {
             <div className="flex justify-cetner items-center">            
                 <h2 className="text-3xl font-sans text-[#313131]">bootcamp fullstack | Cilsa</h2>
                 <div className="w-44 ml-6 text-[#313131] rounded-full font-medium text-3xl">
-                    <button className="text-[#313131] border border-[#313131] hover:border-0 tracking-wide font-sans font-medium py-3 px-5 rounded-2xl text-lg hover:bg-[#559933]">Agrear tarea</button>
+                    <button className="text-[#313131] border border-[#313131] hover:border-0 tracking-wide font-sans font-medium py-3 px-5 rounded-2xl text-xl hover:bg-[#559933]">Agrear tarea</button>
                 </div>
             </div>
             <div className="mt-12">
-                <table className="bg-[#8FD14F] text-gray-700 border border-gray-300 rounded-2xl shadow-md">
+                <table className="text-[#313131] border border-gray-400 shadow-md table">
                     <thead>
-                        <tr className="bg-gray-50">
-                            <th className="px-6 py-4 font-medium text-left">Nombre</th>
-                            <th className="px-44 py-4 font-medium text-left">Descripción</th>
+                        <tr className="bg-gray-400">
+                            <th className="px-6 py-4 font-medium text-center text-xl text-[#313131]">Nombre</th>
+                            <th className="px-6 py-4 font-medium text-center text-xl text-[#313131]">Descripción</th>
+                            <th className="px-6 py-4 font-medium text-center text-xl text-[#313131">Acción</th>
                         </tr>
                     </thead>
                     <tbody>
-
+                        {tareas.map((data, i) => (
+                            <tr key={data.id || i}> {/* Use data.id if available, otherwise use index */}
+                                <td className="pr-40 text-left">{data.nombre}</td>
+                                <td className="pr-40 text-left">{data.descripcion}</td>
+                                <td>
+                                <div className="flex justify-between items-center"> {/* Added a flex container */}
+                                        <button className="text-[#313131] border border-[#313131] hover:border-0 mr-8 tracking-wide font-sans font-medium py-3 px-5 rounded-2xl text-lg hover:bg-[#7a9cfa]">Update</button>
+                                        <button className="text-[#313131] border border-[#313131] hover:border-0 tracking-wide font-sans font-medium py-3 px-5 rounded-2xl text-lg hover:bg-[#fc2121]">Delete</button>
+                                    </div>
+                                </td>
+                            </tr>
+                        ))}
                     </tbody>
                 </table>
             </div>
